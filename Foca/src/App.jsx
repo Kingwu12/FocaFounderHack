c2d77dd4f8381acb191ffea1f6fb497f26ec881b
King Wu <154056545+Kingwu12@users.noreply.github.com>
2024-08-03 16:22:49 +1000

    update

--- Foca/src/App.jsx
+++ Foca/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import AuthPage from './pages/AuthPage/AuthPage';
 import PageLayout from './layouts/PageLayout';
@@ -6,10 +6,13 @@ import ProfilePage from './pages/ProfilePage/ProfilePage';
 import FocusDashBoard from './pages/FocusDashBoard/FocusDashBoard';
 import FocusFeedPage from './pages/FocusFeedPage/FocusFeedPage';
 
+const defaultUsername = 'Heisenberg';
+
 function App() {
   return (
     <PageLayout>
       <Routes>
+        <Route path='/' element={<Navigate to={`/dashboard/${defaultUsername}`} />} />
         <Route path='/dashboard/:username' element={<FocusDashBoard />} />
         <Route path='/auth' element={<AuthPage />} />
         <Route path='/profile/:username' element={<ProfilePage />} />
