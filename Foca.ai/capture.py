d9471f158dbb34bfc2494504669583a3185d62fc
SanketPai0518 <111495475+SanketPai0518@users.noreply.github.com>
2024-08-03 17:07:17 +1000

    calcualte time spent file added which roughly calculates the time spent on each application

--- Foca.ai/capture.py
+++ Foca.ai/capture.py
@@ -12,7 +12,7 @@ def capture_screen():
         img = Image.frombytes('RGB', screenshot.size, screenshot.bgra, 'raw', 'BGRX')
         return img
 
-def resize_image(img, max_size=(400, 400)):
+def resize_image(img, max_size=(500, 500)):
     img.thumbnail(max_size)
     return img
 
