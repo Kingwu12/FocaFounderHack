a53446893a7b284b0a1427bb44359978c891c4d2
CCharles99 <ccha0057@student.monash.edu>
2024-08-03 17:56:57 +1000

    timer half-baked

--- Foca/src/pages/FocusDashBoard/FocusDashBoard.jsx
+++ Foca/src/pages/FocusDashBoard/FocusDashBoard.jsx
@@ -1,13 +1,15 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import TaskManager from '../../components/TaskManager/TaskManager';
+import Timer from '../../components/Timer/Timer';
 import { useParams } from 'react-router-dom';
 
 const FocusDashBoard = () => {
   const { username } = useParams();
 
   return (
-    <Flex w='100vw' flexDir='column' h='100%'>
+    <Flex w='100vw' flexDir='row' h='100%'>
       <TaskManager username={username} />
+      <Timer />
     </Flex>
   );
 };
